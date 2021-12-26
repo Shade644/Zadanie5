d0bb8abc85172de4486fca51c8a1bec6b14cbff8
Adrian Włodarczyk <33695278+Shade644@users.noreply.github.com>
2021-12-26 15:28:00 +0100

    .
    .

---
+++ Zadanie6/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+
+namespace Zadanie6.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        public DbSet<TodoApi.Models.TodoItem> TodoItem { get; set; }
+    }
+}
